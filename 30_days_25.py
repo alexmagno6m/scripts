9073cb45fef8d5136913d82d04baa8a8ab8bd482
alexmagno6m <alexacevedog@gmail.com>
2021-04-17 00:59:16 -0500

    formatted file pep 8

--- 30_days_25.py
+++ 30_days_25.py
@@ -1,5 +1,6 @@
-#day 25 hackerrank
+# day 25 hackerrank
 from math import sqrt
+
 t = int(input())
 numbers = []
 for j in range(t):
@@ -8,15 +9,15 @@ for j in range(t):
 l = len(numbers)
 for j in range(l):
     n = numbers[j]
-    len =  int(sqrt(n))
+    len = int(sqrt(n))
     result = True
     acum = 0
     if n == 1:
         print('Not prime')
-    elif n>1 and  n <= 3:
+    elif 1 < n <= 3:
         print('Prime')
     else:
-        for j in range(2,len+1):
+        for j in range(2, len + 1):
             if n % j == 0:
                 prime = False
                 print('Not prime')
@@ -24,5 +25,5 @@ for j in range(l):
             else:
                 prime = True
                 acum += 1
-        if acum>=1 and prime==True:
+        if acum >= 1 and prime == True:
             print('Prime')

fea7bab2c3ec8ad83d772125b407b5d42ae6b726
alexmagno6m <alexacevedog@gmail.com>
2021-10-10 00:16:43 -0500

    new octuber

---
+++ 10_prog/clase1_3p.py
@@ -0,0 +1,29 @@
+from tkinter import *
+ventana = Tk()
+ventana.minsize(500, 500)
+ventana.config(bg="blue")
+ventana.title('muestra')
+ventana.mainloop()
+'''
+Tarea:
+1. Haga un listado de las palabras reservadas de Pyhton
+2. Indique la funcion de las siguientes expresiones de Python
+a) print
+b) return
+c) import
+3. Indique en programacion los siguientes conceptos
+a) Errores de tiempo de ejecucion
+b) Errores semanticos
+c) Errores sintacticos
+4. Sobre las cadenas en Python indique:
+a) que son los indices
+b) que es la longitud
+c) haga un listado de 5 metodos con cadenas
+Fecha: 11 de agosto antes de las 6PM en alexacevedog@icloud.com
+'''
+
+
+
+
+
+
---
+++ clase 28 julio.py
@@ -0,0 +1,21 @@
+def saludo2():
+    mensaje = "hola mundo"
+    return mensaje
+print(saludo2())
+
+'''Tarea
+1. Investigue en que consiste el intercambio de variables
+de funciones en programacion
+2. Sobre los diccionarios en Python explique:
+a) Que son
+b) Cuales son sus componentes
+c) Nombre los metodos para obtener las claves
+y los valores
+Fecha: lunes 9 de agosto a alexacevedog@icloud.com
+
+
+
+
+
+'''
+
